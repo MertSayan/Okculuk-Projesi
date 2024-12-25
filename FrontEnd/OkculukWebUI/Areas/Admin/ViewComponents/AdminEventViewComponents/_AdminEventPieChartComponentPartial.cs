7395a1fa760aabe520d17281b905032b7effc690
Mert Sayan <Mertrutenyumsayan@gmail.com>
2024-12-25 16:19:09 +0300

    Admin Event kısmına piechart ekledim

---
+++ FrontEnd/OkculukWebUI/Areas/Admin/ViewComponents/AdminEventViewComponents/_AdminEventPieChartComponentPartial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using OkculukDto.EventDtos;
+
+namespace OkculukWebUI.Areas.Admin.ViewComponents.AdminEventViewComponents
+{
+    public class _AdminEventPieChartComponentPartial:ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _AdminEventPieChartComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int id)
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7082/api/Events/GetEventsCountByStatusAndEventId?eventId="+id);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var JsonData=await responseMessage.Content.ReadAsStringAsync();
+                var values=JsonConvert.DeserializeObject<AdminResultEventsStatusCount>(JsonData);
+                return View(values);
+            }
+
+            return View();
+        }
+    }
+}
